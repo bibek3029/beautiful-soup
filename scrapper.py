00d12961c1e0db95544b4570fb5aacf14b82d906
bibek <bibeksubedi1998@gmail.com>
2020-02-29 05:05:57 +0530

    final commit

--- scrapper.py
+++ scrapper.py
@@ -6,20 +6,30 @@ myurlvar = uReq(my_url)
 page_html = myurlvar.read()
 myurlvar.close()
 page_soup = soup(page_html, "html.parser")
+#this takes you to to get title of the page
 a = page_soup.title.text
 print(a)
 #grab div class which consist of post summary
 #myclass = page_soup.findAll("div",{"class": "post-summary"}
+#here we get the first paragram text
 myclass = page_soup.find('div', class_='post-summary').p.text
 print(myclass)
+#sep variable gives you the time 8th june 2020
 sep = page_soup.findAll('time')
 print(sep[0])
+#now scrapping netherland from the website
 netherland = page_soup.find('div', class_='location-details').p.text
 print(netherland)
+#for getting political science, deeplearning all five from below
 taggs = page_soup.find('ul', {'id':'tags'})
 print(taggs.text)
-summer = page_soup.findAll("div",{"class": "post-details hide-for-small-only"})
-print(summer)
+#now to get summer school we go through below process
+myhead = page_soup.find('div', {'class':'post-details hide-for-small-only'}, 'div')
+a= myhead.findAll('h4')
+print(a[4])
+#now for getting website link we go through following process
+wwebsite = page_soup.find('div', class_='small-16 apply')
+print(wwebsite.p.a)
 
 
 
